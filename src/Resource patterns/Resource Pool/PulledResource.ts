09ebb76c6328036fac0fee093697fdcd9fadde0c
Zybr <zybrof@gmail.com>
2022-12-06 11:01:34 +0300

    Resource Timer

--- src/Resource patterns/Resource Pool/PulledResource.ts
+++ src/Resource patterns/Resource Pool/PulledResource.ts
@@ -1,17 +1,21 @@
 import IResource from "./IResource";
 import ResourcePool from "./ResourcePool";
+import ResourceTimer from "./ResourceTimer";
 
 export default class PulledResource implements IResource {
   private readonly pool: ResourcePool;
   private db: IResource | null;
+  private readonly LIFE_TIME = 1000; // 1 second
+  private readonly timer: ResourceTimer;
 
   public constructor(pool: ResourcePool, db: IResource) {
     this.pool = pool;
     this.db = db;
+    this.timer = new ResourceTimer(this.LIFE_TIME, this.close.bind(this));
   }
 
   public selectOne(sql: string, params: []): Promise<[] | null> {
-    this.checkStatus();
+    this.syncStatus();
 
     return this.db.selectOne(sql, params);
   }
@@ -22,9 +26,11 @@ export default class PulledResource implements IResource {
     return this;
   }
 
-  private checkStatus(): void {
+  private syncStatus(): void {
     if (!this.db) {
       throw new Error('Resource is closed.');
     }
+
+    this.timer.reset(this.LIFE_TIME);
   }
 }
--- src/Resource patterns/Resource Pool/ResourcePool.test.ts
+++ src/Resource patterns/Resource Pool/ResourcePool.test.ts
@@ -2,15 +2,16 @@ import ResourcePool from "./ResourcePool";
 import { commonDbm } from "../../../database/databases";
 
 const DB_PATH = commonDbm.getDbPath();
+const SQL = "SELECT COUNT(*) as count FROM products"
 
 describe('DbPool', () => {
   beforeAll(async () => commonDbm.init());
 
-  test('Get one resource', async () => {
+  test('Get a resource', async () => {
     const pool = new ResourcePool(3, DB_PATH);
 
     const resource = pool.getResource();
-    const row = await resource.selectOne("SELECT COUNT(*) as count FROM products", []);
+    const row = await resource.selectOne(SQL, []);
     expect(row['count']).not.toBeUndefined();
   });
 
@@ -20,21 +21,21 @@ describe('DbPool', () => {
 
     for (let i = 0; i < connectionsNum; i++) {
       const resource = pool.getResource();
-      const row = await resource.selectOne("SELECT COUNT(*) as count FROM products", []);
+      const row = await resource.selectOne(SQL, []);
       expect(row['count']).not.toBeUndefined();
     }
 
     expect(() => pool.getResource()).toThrow(new Error('There are not available resources.'));
   });
 
-  test('Get and close resource', async () => {
+  test('Close resource', async () => {
     const connectionsNum = 1;
     const pool = new ResourcePool(connectionsNum, DB_PATH);
 
     // Make resource
 
     const resourceA = pool.getResource();
-    const rowA = await resourceA.selectOne("SELECT COUNT(*) as count FROM products", []);
+    const rowA = await resourceA.selectOne(SQL, []);
     expect(rowA['count']).not.toBeUndefined();
     expect(() => pool.getResource()).toThrow(new Error('There are not available resources.'));
 
@@ -46,7 +47,24 @@ describe('DbPool', () => {
     // Make new resource from released connection
 
     const resourceB = pool.getResource();
-    const rowB = await resourceB.selectOne("SELECT COUNT(*) as count FROM products", []);
+    const rowB = await resourceB.selectOne(SQL, []);
     expect(rowB['count']).not.toBeUndefined();
+  });
+
+  test('Expire resource', async () => {
+    const connectionsNum = 1;
+    const pool = new ResourcePool(connectionsNum, DB_PATH);
+
+    // Make resource
+
+    const resourceA = pool.getResource();
+    const rowA = await resourceA.selectOne(SQL, []);
+    expect(rowA['count']).not.toBeUndefined();
+
+    // Expect unit resource expire
+
+    await new Promise(resolve => setTimeout(() => resolve(null), 1100));
+
+    expect(() => resourceA.selectOne("", [])).toThrow(new Error('Resource is closed.'));
   });
 });
